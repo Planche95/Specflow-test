0279fa33f811ee759e88b4e9fce109e46f5c6eef
Tomasz Kaniewski <tomasz.kaniewski@cybercom.com>
2019-02-27 15:35:20 +0100

    Initial commit

---
+++ SpecflowTest/Features/SpecFlowFeature1.feature.cs
@@ -0,0 +1,26 @@
+﻿We could not find a data exchange file at the path System.Configuration.ConfigurationErrorsException: SpecFlow configuration error ---> System.Xml.XmlException: Data at the root level is invalid. Line 1, position 1.
+
+Please open an issue at https://github.com/techtalk/SpecFlow/issues/
+Complete output: 
+System.Configuration.ConfigurationErrorsException: SpecFlow configuration error ---> System.Xml.XmlException: Data at the root level is invalid. Line 1, position 1.
+   at System.Xml.XmlTextReaderImpl.Throw(Exception e)
+   at System.Xml.XmlTextReaderImpl.Throw(String res, String arg)
+   at System.Xml.XmlTextReaderImpl.ParseRootLevelWhitespace()
+   at System.Xml.XmlTextReaderImpl.ParseDocumentContent()
+   at System.Xml.XmlTextReaderImpl.Read()
+   at System.Xml.XmlTextReader.Read()
+   at System.Configuration.ConfigurationSection.DeserializeSection(XmlReader reader)
+   at TechTalk.SpecFlow.Configuration.ConfigurationSectionHandler.CreateFromXml(String xmlContent)
+   at TechTalk.SpecFlow.Generator.Configuration.GeneratorConfigurationProvider.GetPlugins(SpecFlowConfigurationHolder configurationHolder)
+   --- End of inner exception stack trace ---
+   at TechTalk.SpecFlow.Generator.Configuration.GeneratorConfigurationProvider.GetPlugins(SpecFlowConfigurationHolder configurationHolder)
+   at TechTalk.SpecFlow.Generator.GeneratorContainerBuilder.LoadPlugins(ObjectContainer container, IGeneratorConfigurationProvider configurationProvider, SpecFlowConfigurationHolder configurationHolder)
+   at TechTalk.SpecFlow.Generator.GeneratorContainerBuilder.CreateContainer(SpecFlowConfigurationHolder configurationHolder, ProjectSettings projectSettings)
+   at TechTalk.SpecFlow.Generator.TestGeneratorFactory.CreateGenerator(ProjectSettings projectSettings)
+   at TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.Actions.GenerateTestFileAction.GenerateTestFile(GenerateTestFileParameters opts)
+
+
+
+Command: c:\users\tokan1\appdata\local\microsoft\visualstudio\15.0_f56a7326\extensions\sszmbupi.gnp\TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.exe
+Parameters: GenerateTestFile --featurefile C:\Users\tokan1\AppData\Local\Temp\tmp3326.tmp --outputdirectory C:\Users\tokan1\AppData\Local\Temp --projectsettingsfile C:\Users\tokan1\AppData\Local\Temp\tmp3316.tmp
+Working Directory: 
---
+++ SpecflowTest/Steps/SpecFlowFeature1Steps.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace SpecflowTest.Steps
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private List<int> values = new List<int>();
+        private int result;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int value)
+        {
+            values.Add(value);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = values.Sum();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int value)
+        {
+            result.Should().Be(value);
+        }
+    }
+}
